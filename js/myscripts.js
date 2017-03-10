fcbc01d643849fe5fd99e5e2febaeb908c0fdcc4
Nathalie Marquesin <nmarquesin@live.com>
2017-03-10 16:17:39 +0000

    Placeholders in white

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,24 +1,31 @@
 
 var gameStarted = false;
-var player;
-var computer;
-var board = { "fieldA1": false, "fieldA2": false, "fieldA3": false, "fieldB1": false, "fieldB2": false, "fieldB3": false, "fieldC1": false, "fieldC2": false, "fieldC3": false}
-var boardPlayer = { "fieldA1": 0, "fieldA2": 0, "fieldA3": 0, "fieldB1": 0, "fieldB2": 0, "fieldB3": 0, "fieldC1": 0, "fieldC2": 0, "fieldC3": 0}
-var boardAI = { "fieldA1": 0, "fieldA2": 0, "fieldA3": false, "fieldB1": false, "fieldB2": false, "fieldB3": false, "fieldC1": false, "fieldC2": false, "fieldC3": false}
+var player; // player chooses beween X or O
+var computer; // computer gets remaining symbol
+var board; // tic tac toe logic board
+var boardPlayer; // player's plays
+var boardAI; // AI's plays
 var whoseTurnIsiT = false; // false => player, true => computer
 
 
 function startGame() {
-  board = { "fieldA1": false, "fieldA2": false, "fieldA3": false, "fieldB1": false, "fieldB2": false, "fieldB3": false, "fieldC1": false, "fieldC2": false, "fieldC3": false}
+  board = { "fieldA1": false, "fieldA2": false, "fieldA3": false, "fieldB1": false, "fieldB2": false, "fieldB3": false, "fieldC1": false, "fieldC2": false, "fieldC3": false};
+  boardPlayer = { "fieldA1": 0, "fieldA2": 0, "fieldA3": 0, "fieldB1": 0, "fieldB2": 0, "fieldB3": 0, "fieldC1": 0, "fieldC2": 0, "fieldC3": 0};
+  boardAI = { "fieldA1": 0, "fieldA2": 0, "fieldA3": 0, "fieldB1": 0, "fieldB2": 0, "fieldB3": 0, "fieldC1": 0, "fieldC2": 0, "fieldC3": 0};
   gameStarted = true;
   whoseTurnIsiT = true;
   console.log("Game has started. Player selected: "+player+" . Computer plays "+computer);
+  for (var key in board) {
+    $("#"+key).html("<span class='placeholder'>PH<span>");
+    $("#message").html("<p>Computer's turn</p>");
+  }
 }
 
 function setPlayerO() {
   if (gameStarted === false) {
     player = "O";
     computer = "X";
+    $("#whichiswhich").html('<span class="R">Computer: X </span>|||<span class="B"> Player: O</span>');
     startGame();
   }
 }
@@ -27,6 +34,7 @@ function setPlayerX() {
   if (gameStarted === false) {
     player = "X";
     computer = "O";
+    $("#whichiswhich").html('<span class="R">Player: X </span>|||<span class="B"> Computer: O</span>');
     startGame();
   }
 }
@@ -42,8 +50,9 @@ function myFunction(field, playerTurn) {
         boardAI[field] = 1;
       }
       console.log("Board "+field+" is now "+board[field]);
-      checkWin();
       whoseTurnIsiT = true;
+      $("#message").html("<p>Computer's turn</p>");
+      checkWin();
     }
   }
 }
@@ -116,97 +125,94 @@ function checkWin() {
     computerWins();
     break;
   default:
-}
-switch (boardAI["fieldB1"] + boardAI["fieldB2"] + boardAI["fieldB3"]) {
-  case 3:
-    computerWins();
-    break;
-  default:
-}
-switch (boardAI["fieldC1"] + boardAI["fieldC2"] + boardAI["fieldC3"]) {
-  case 3:
-    computerWins();
-    break;
-  default:
-}
-switch (boardAI["fieldA1"] + boardAI["fieldB1"] + boardAI["fieldC1"]) {
-  case 3:
-    computerWins();
-    break;
-  default:
-}
-switch (boardAI["fieldA2"] + boardAI["fieldB2"] + boardAI["fieldC2"]) {
-  case 3:
-    computerWins();
-    break;
-  default:
-}
-switch (boardAI["fieldA3"] + boardAI["fieldB3"] + boardAI["fieldC3"]) {
-  case 3:
-    computerWins();
-    break;
-  default:
-}
-switch (boardAI["fieldA1"] + boardAI["fieldB2"] + boardAI["fieldC3"]) {
-  case 3:
-    computerWins();
-    break;
-  default:
-}
-switch (boardAI["fieldA3"] + boardAI["fieldB2"] + boardAI["fieldC1"]) {
-  case 3:
-    computerWins();
-    break;
-  default:
-}
-
-
-
+  }
+  switch (boardAI["fieldB1"] + boardAI["fieldB2"] + boardAI["fieldB3"]) {
+    case 3:
+      computerWins();
+      break;
+    default:
+  }
+  switch (boardAI["fieldC1"] + boardAI["fieldC2"] + boardAI["fieldC3"]) {
+    case 3:
+      computerWins();
+      break;
+    default:
+  }
+  switch (boardAI["fieldA1"] + boardAI["fieldB1"] + boardAI["fieldC1"]) {
+    case 3:
+      computerWins();
+      break;
+    default:
+  }
+  switch (boardAI["fieldA2"] + boardAI["fieldB2"] + boardAI["fieldC2"]) {
+    case 3:
+      computerWins();
+      break;
+    default:
+  }
+  switch (boardAI["fieldA3"] + boardAI["fieldB3"] + boardAI["fieldC3"]) {
+    case 3:
+      computerWins();
+      break;
+    default:
+  }
+  switch (boardAI["fieldA1"] + boardAI["fieldB2"] + boardAI["fieldC3"]) {
+    case 3:
+      computerWins();
+      break;
+    default:
+  }
+  switch (boardAI["fieldA3"] + boardAI["fieldB2"] + boardAI["fieldC1"]) {
+    case 3:
+      computerWins();
+      break;
+    default:
+  }
 }
 
 function myFunctionA1() {
   var field = "fieldA1";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 function myFunctionA2() {
   var field = "fieldA2";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 function myFunctionA3() {
   var field = "fieldA3";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 function myFunctionB1() {
   var field = "fieldB1";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 function myFunctionB2() {
   var field = "fieldB2";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 function myFunctionB3() {
   var field = "fieldB3";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 function myFunctionC1() {
   var field = "fieldC1";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 function myFunctionC2() {
   var field = "fieldC2";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 function myFunctionC3() {
   var field = "fieldC3";
-  myFunction(field, player);
+  if (whoseTurnIsiT === false) {myFunction(field, player);}
 }
 
 
@@ -216,8 +222,8 @@ function nextAvailable() {
     if (board[key] === false) {
       console.log(key + " -> " + board[key]);
       return key;
+    }
   }
-}
 }
 
 $(document).ready(function(){
@@ -227,11 +233,10 @@ $(document).ready(function(){
       myFunction(nextField, computer);
       console.log(board);
       whoseTurnIsiT = false;
-    } else if (gameStarted === true && whoseTurnIsiT === false) {
-
+      $("#message").html("<p>Player's turn</p>");
+      checkWin();
     }
 
+  }, 3000);
 
-  }, 2000);
-
-}, 1000);
+});
